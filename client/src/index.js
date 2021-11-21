8785c18d6e4264815dca88715e063154b47a1b5f
Pablo <cruzpablomartin@gmail.com>
2021-11-20 21:10:52 -0300

    Add Redux Provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
+import store from './store/index';
 import App from './App';
 
 // BOOTSWATCH CSS
 import 'bootswatch/dist/minty/bootstrap.min.css';
 // BOOTSTRAP JS
-import 'bootstrap/dist/js/bootstrap.bundle';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 // CUSTOM CSS
 import './index.css';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
