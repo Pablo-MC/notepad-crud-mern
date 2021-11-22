7d3dd2ba5ebef0de56a6f324b14ea9f1c2f748fe
Pablo <cruzpablomartin@gmail.com>
2021-11-22 19:26:37 -0300

    Fix Navbar

--- client/src/components/Layout/Navbar.js
+++ client/src/components/Layout/Navbar.js
@@ -1,27 +1,37 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { authActions } from '../../store/auth/auth-slice';
 
-const Navbar = (props) => {
+const Navbar = () => {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const dispatch = useDispatch();
+
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+
+  useEffect(() => {
+    const storedUserLoggedIn = sessionStorage.getItem('isLoggedIn');
+    if (storedUserLoggedIn === '1') setIsLoggedIn(true);
+  }, [isAuthenticated]);
+
+  const logoutHandler = () => {
+    dispatch(authActions.loguot());
+    setIsLoggedIn(false);
+  }
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-primary px-2">
-      <div className="container-fluid">
-        <Link to='/' className="navbar-brand">Notepad</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <Link to='/' className="nav-link active me-4">Create Note</Link>
-            </li>
-            <li className="nav-item">
-              {props.showLogin
-                ? <Link to='/register' className="btn btn-dark">Register</Link>
-                : <Link to='/' className="btn btn-dark">Login</Link>
-              }
-            </li>
-          </ul>
-        </div>
-      </div>
+    <nav className="navbar navbar-light bg-primary px-3">
+      <Link to='/' className="navbar-brand"><strong>Notepad</strong></Link>
+      <ul className="navbar-nav ms-auto">
+        <li className="nav-item">
+          {isLoggedIn
+            ? <Link to='/' className="btn btn-dark ms-3" onClick={logoutHandler}>Log Out</Link>
+            : <Link to='/login' className="btn btn-dark">Login</Link>
+          }
+        </li>
+      </ul>
     </nav>
   );
 }
